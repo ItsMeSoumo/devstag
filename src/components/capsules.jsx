15065f920f015ccc67ef530aaf0654dc8ee6e7f4
SOUMO <soumo2020.saha@gmail.com>
2025-09-03 20:21:45 +0530

    gh

--- src/components/capsules.jsx
+++ src/components/capsules.jsx
@@ -8,178 +8,179 @@ import { SplitText } from "gsap/SplitText";
 import { Spotlight } from "@/components/ui/spotlight";
 import LightRays from "@/components/ui/lightrays";
 import Squares from "@/components/ui/movingsquares";
-
+import Plasma from "@/components/ui/plasma";
+import Orb from "@/components/ui/orb";
 
 export default function Capsules() {
   const container = useRef();
-    useEffect(() => {
+  useEffect(() => {
     gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
-  const cards = gsap.utils.toArray(".card");
-  const introCard = cards[0];
+    const cards = gsap.utils.toArray(".card");
+    const introCard = cards[0];
 
-  const titles = gsap.utils.toArray(".card-title h1");
-  if (titles.length) {
-    titles.forEach((title) => {
-      const split = new SplitText(title, {
-        type: "chars",
-        charsClass: "char",
-        tag: "div",
-      });
-      split.chars.forEach((char) => {
-        char.innerHTML = `<span>${char.textContent}</span>`;
-      });
-    });
-  }
-
-  const cardImgWrapper = introCard.querySelector(".card-img");
-  const cardImg = introCard.querySelector(".card-img img");
-  gsap.set(cardImgWrapper, { scale: 0.5, borderRadius: "400px" });
-  gsap.set(cardImg, { scale: 1.5 });
-
-  function animateContentIn(titleChars, description) {
-    gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
-    gsap.to(description, {
-      x: 0,
-      opacity: 1,
-      duration: 0.75,
-      delay: 0.1,
-      ease: "power4.out",
-    });
-  }
-
-  function animateContentOut(titleChars, description) {
-    gsap.to(titleChars, { x: "100%", duration: 0.5, ease: "power4.out" });
-    gsap.to(description, {
-      x: "40px",
-      opacity: 0,
-      duration: 0.5,
-      ease: "power4.out",
-    });
-  }
-
-  const marquee = introCard.querySelector(".card-marquee .marquee");
-  const titleChars = introCard.querySelectorAll(".char span");
-  const description = introCard.querySelector(".card-description");
-
-  ScrollTrigger.create({
-    trigger: introCard,
-    start: "top top",
-    end: "+=300vh",
-    onUpdate: (self) => {
-      const progress = self.progress;
-      const imgScale = 0.5 + progress * 0.5;
-      const borderRadius = 400 - progress * 375;
-      const innerImgScale = 1.5 - progress * 0.5;
-
-      gsap.set(cardImgWrapper, {
-        scale: imgScale,
-        borderRadius: borderRadius + "px",
-      });
-      gsap.set(cardImg, { scale: innerImgScale });
-
-      if (imgScale >= 0.5 && imgScale <= 0.75) {
-        const fadeProgress = (imgScale - 0.5) / (0.75 - 0.5);
-        gsap.set(marquee, { opacity: 1 - fadeProgress });
-      } else if (imgScale < 0.5) {
-        gsap.set(marquee, { opacity: 1 });
-      } else if (imgScale > 0.75) {
-        gsap.set(marquee, { opacity: 0 });
-      }
-
-      if (progress >= 1 && !introCard.contentRevealed) {
-        introCard.contentRevealed = true;
-        animateContentIn(titleChars, description);
-      }
-      if (progress < 1 && introCard.contentRevealed) {
-        introCard.contentRevealed = false;
-        animateContentOut(titleChars, description);
-      }
-    },
-  });
-
-  cards.forEach((card, index) => {
-    const isLastCard = index === cards.length - 1;
-    ScrollTrigger.create({
-      trigger: card,
-      start: "top top",
-      end: isLastCard ? "+=100vh" : "top top",
-      endTrigger: isLastCard ? null : cards[cards.length - 1],
-      pin: true,
-      pinSpacing: isLastCard,
-    });
-  });
-
-  cards.forEach((card, index) => {
-    if (index < cards.length - 1) {
-      const cardWrapper = card.querySelector(".card-wrapper");
-      ScrollTrigger.create({
-        trigger: cards[index + 1],
-        start: "top bottom",
-        end: "top top",
-        onUpdate: (self) => {
-          const progress = self.progress;
-          gsap.set(cardWrapper, {
-            scale: 1 - progress * 0.25,
-            opacity: 1 - progress,
-          });
-        },
+    const titles = gsap.utils.toArray(".card-title h1");
+    if (titles.length) {
+      titles.forEach((title) => {
+        const split = new SplitText(title, {
+          type: "chars",
+          charsClass: "char",
+          tag: "div",
+        });
+        split.chars.forEach((char) => {
+          char.innerHTML = `<span>${char.textContent}</span>`;
+        });
       });
     }
-  });
 
-  cards.forEach((card, index) => {
-    if (index > 0) {
-      const cardImg = card.querySelector(".card-img img");
-      const imgContainer = card.querySelector(".card-img");
+    const cardImgWrapper = introCard.querySelector(".card-img");
+    const cardImg = introCard.querySelector(".card-img img");
+    gsap.set(cardImgWrapper, { scale: 0.5, borderRadius: "400px" });
+    gsap.set(cardImg, { scale: 1.5 });
+
+    function animateContentIn(titleChars, description) {
+      gsap.to(titleChars, { x: "0%", duration: 0.75, ease: "power4.out" });
+      gsap.to(description, {
+        x: 0,
+        opacity: 1,
+        duration: 0.75,
+        delay: 0.1,
+        ease: "power4.out",
+      });
+    }
+
+    function animateContentOut(titleChars, description) {
+      gsap.to(titleChars, { x: "100%", duration: 0.5, ease: "power4.out" });
+      gsap.to(description, {
+        x: "40px",
+        opacity: 0,
+        duration: 0.5,
+        ease: "power4.out",
+      });
+    }
+
+    const marquee = introCard.querySelector(".card-marquee .marquee");
+    const titleChars = introCard.querySelectorAll(".char span");
+    const description = introCard.querySelector(".card-description");
+
+    ScrollTrigger.create({
+      trigger: introCard,
+      start: "top top",
+      end: "+=300vh",
+      onUpdate: (self) => {
+        const progress = self.progress;
+        const imgScale = 0.5 + progress * 0.5;
+        const borderRadius = 400 - progress * 375;
+        const innerImgScale = 1.5 - progress * 0.5;
+
+        gsap.set(cardImgWrapper, {
+          scale: imgScale,
+          borderRadius: borderRadius + "px",
+        });
+        gsap.set(cardImg, { scale: innerImgScale });
+
+        if (imgScale >= 0.5 && imgScale <= 0.75) {
+          const fadeProgress = (imgScale - 0.5) / (0.75 - 0.5);
+          gsap.set(marquee, { opacity: 1 - fadeProgress });
+        } else if (imgScale < 0.5) {
+          gsap.set(marquee, { opacity: 1 });
+        } else if (imgScale > 0.75) {
+          gsap.set(marquee, { opacity: 0 });
+        }
+
+        if (progress >= 1 && !introCard.contentRevealed) {
+          introCard.contentRevealed = true;
+          animateContentIn(titleChars, description);
+        }
+        if (progress < 1 && introCard.contentRevealed) {
+          introCard.contentRevealed = false;
+          animateContentOut(titleChars, description);
+        }
+      },
+    });
+
+    cards.forEach((card, index) => {
+      const isLastCard = index === cards.length - 1;
       ScrollTrigger.create({
         trigger: card,
-        start: "top bottom",
-        end: "top top",
-        onUpdate: (self) => {
-          const progress = self.progress;
-          gsap.set(cardImg, { scale: 2 - progress });
-          gsap.set(imgContainer, { borderRadius: 150 - progress * 125 + "px" });
-        },
+        start: "top top",
+        end: isLastCard ? "+=100vh" : "top top",
+        endTrigger: isLastCard ? null : cards[cards.length - 1],
+        pin: true,
+        pinSpacing: isLastCard,
       });
+    });
+
+    cards.forEach((card, index) => {
+      if (index < cards.length - 1) {
+        const cardWrapper = card.querySelector(".card-wrapper");
+        ScrollTrigger.create({
+          trigger: cards[index + 1],
+          start: "top bottom",
+          end: "top top",
+          onUpdate: (self) => {
+            const progress = self.progress;
+            gsap.set(cardWrapper, {
+              scale: 1 - progress * 0.25,
+              opacity: 1 - progress,
+            });
+          },
+        });
+      }
+    });
+
+    cards.forEach((card, index) => {
+      if (index > 0) {
+        const cardImg = card.querySelector(".card-img img");
+        const imgContainer = card.querySelector(".card-img");
+        ScrollTrigger.create({
+          trigger: card,
+          start: "top bottom",
+          end: "top top",
+          onUpdate: (self) => {
+            const progress = self.progress;
+            gsap.set(cardImg, { scale: 2 - progress });
+            gsap.set(imgContainer, { borderRadius: 150 - progress * 125 + "px" });
+          },
+        });
+      }
+    });
+
+    cards.forEach((card, index) => {
+      if (index === 0) return;
+
+      const cardDescription = card.querySelector(".card-description");
+      const cardTitleChars = card.querySelectorAll(".char span");
+
+      ScrollTrigger.create({
+        trigger: card,
+        start: "top top",
+        onEnter: () => animateContentIn(cardTitleChars, cardDescription),
+        onLeaveBack: () => animateContentOut(cardTitleChars, cardDescription),
+      });
+    });
+    // Play/pause YouTube iframes per card visibility
+    function sendYTCommand(iframe, func) {
+      if (!iframe || !iframe.contentWindow) return;
+      iframe.contentWindow.postMessage(
+        JSON.stringify({ event: "command", func, args: [] }),
+        "*"
+      );
     }
-  });
-
-  cards.forEach((card, index) => {
-    if (index === 0) return;
-
-    const cardDescription = card.querySelector(".card-description");
-    const cardTitleChars = card.querySelectorAll(".char span");
-
-    ScrollTrigger.create({
-      trigger: card,
-      start: "top top",
-      onEnter: () => animateContentIn(cardTitleChars, cardDescription),
-      onLeaveBack: () => animateContentOut(cardTitleChars, cardDescription),
+    cards.forEach((card) => {
+      const iframe = card.querySelector("iframe");
+      if (!iframe) return;
+      ScrollTrigger.create({
+        trigger: card,
+        start: "top center",
+        end: "bottom center",
+        onEnter: () => sendYTCommand(iframe, "playVideo"),
+        onEnterBack: () => sendYTCommand(iframe, "playVideo"),
+        onLeave: () => sendYTCommand(iframe, "pauseVideo"),
+        onLeaveBack: () => sendYTCommand(iframe, "pauseVideo"),
+      });
     });
-  });
-  // Play/pause YouTube iframes per card visibility
-  function sendYTCommand(iframe, func) {
-    if (!iframe || !iframe.contentWindow) return;
-    iframe.contentWindow.postMessage(
-      JSON.stringify({ event: "command", func, args: [] }),
-      "*"
-    );
-  }
-  cards.forEach((card) => {
-    const iframe = card.querySelector("iframe");
-    if (!iframe) return;
-    ScrollTrigger.create({
-      trigger: card,
-      start: "top center",
-      end: "bottom center",
-      onEnter: () => sendYTCommand(iframe, "playVideo"),
-      onEnterBack: () => sendYTCommand(iframe, "playVideo"),
-      onLeave: () => sendYTCommand(iframe, "pauseVideo"),
-      onLeaveBack: () => sendYTCommand(iframe, "pauseVideo"),
-    });
-  });
     // setupMarqueeAnimation();
 
     return () => ScrollTrigger.getAll().forEach(t => t.kill());
@@ -381,35 +382,38 @@ section {
 
       `}</style>
 
-      {/* Sections */}
-      <section className="intro">
-        {/* LightRays overlay inside intro */}
-        <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
-          <LightRays
-            raysOrigin="top-center"
-            raysColor="#00ffff"
-            raysSpeed={1.5}
-            lightSpread={0.8}
-            rayLength={1.2}
-            followMouse={true}
-            mouseInfluence={0.1}
-            noiseAmount={0.1}
-            distortion={0.05}
-            className="custom-rays"
-          />
-        </div>
-        <h1 style={{ position: 'relative', zIndex: 10 }}>We design spaces that don’t just exist.</h1>
-      </section>
-      <section className="cards">
-          <div className="card">
-            {/* <div className="card-marquee">
-          <div className="marquee">
-            <h1>Design Beyond Boundaries</h1>
-            <h1>Built for Tomorrow</h1>
-            <h1>Real Impact</h1>
-            <h1>Digital Visions</h1>
+        {/* Sections */}
+        <section className="intro">
+          {/* LightRays overlay inside intro */}
+          {/* <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
+            <LightRays
+              raysOrigin="top-center"
+              raysColor="#00ffff"
+              raysSpeed={1.5}
+              lightSpread={0.8}
+              rayLength={1.2}
+              followMouse={true}
+              mouseInfluence={0.1}
+              noiseAmount={0.1}
+              distortion={0.05}
+              className="custom-rays"
+            />
+          </div> */}
+          <div style={{ width: '100%', height: '50vh', position: 'relative', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', textAlign: 'center' }}>
+            <div style={{ width: 320, height: 320 }}>
+              <Orb
+                hoverIntensity={0.5}
+                rotateOnHover={true}
+                hue={0}
+                forceHoverState={false}
+              />
+            </div>
+            <h1 style={{ position: 'relative', zIndex: 10 }}>We design spaces that don’t just exist.</h1>
           </div>
-        </div> */}
+          
+        </section>
+        <section className="cards">
+          {/* <div className="card">
             <div className="card-wrapper">
               <div className="card-content">
                 <div className="card-title"></div>
@@ -426,8 +430,8 @@ section {
                 ></iframe>
               </div>
             </div>
-          </div>
-          <div className="card">
+          </div> */}
+          {/* <div className="card">
             <div className="card-wrapper">
               <div className="card-content">
                 <div className="card-title"></div>
@@ -444,7 +448,7 @@ section {
                 ></iframe>
               </div>
             </div>
-          </div>
+          </div> */}
           <div className="card">
             <div className="card-wrapper">
               <div className="card-content">
@@ -454,7 +458,7 @@ section {
               <div className="card-img">
                 <img src="/card-img-3.jpg" alt="" />
                 <iframe
-                  src="https://www.youtube.com/embed/oWjwktvc2VM?enablejsapi=1&mute=1&playsinline=1&rel=0&modestbranding=1&controls=0&loop=1&playlist=oWjwktvc2VM"
+                  src="https://www.youtube.com/embed/FpuanQiVlkA?enablejsapi=1&mute=1&playsinline=1&rel=0&modestbranding=1&controls=0&loop=1&playlist=FpuanQiVlkA"
                   title="Iro Bond"
                   frameBorder="0"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -502,7 +506,7 @@ section {
             </div>
           </div>
 
-          <div className="card">
+          {/* <div className="card">
             <div className="card-wrapper">
               <div className="card-content">
                 <div className="card-title"></div>
@@ -519,9 +523,9 @@ section {
                 ></iframe>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          <div className="card">
+          {/* <div className="card">
             <div className="card-wrapper">
               <div className="card-content">
                 <div className="card-title"></div>
@@ -538,11 +542,23 @@ section {
                 ></iframe>
               </div>
             </div>
-          </div>
+          </div> */}
         </section>
-    <section className="outro">
-      <h1>Architecture reimagined for the virtual age.</h1>
-    </section>
+        <section className="outro">
+          <section className="relative w-full h-[100vh] overflow-hidden">
+            <Plasma
+              color="#9032c3"
+              speed={0.6}
+              direction="forward"
+              scale={1.1}
+              opacity={0.8}
+              mouseInteractive={false}
+            />
+            <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-5xl font-bold">
+              Architecture reimagined for the virtual age.
+            </h1>
+          </section>
+        </section>
       </div>
     </>
   );
